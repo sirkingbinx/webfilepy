1d73c719a7265a80b964c668f16bdb46e4297b44
SirKingBinx <bendawsdev@gmail.com>
2026-02-03 19:39:49 +0000

    README and stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+# webfilepy
+File browsing application made in Python with flask
+![preview of webfilepy, showing the preview & some buttons](.github/images/image.png)
--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, send_file, Response
+import pathlib
 
 server = Flask(__name__)
 
-@server.route("/")
-def home():
-    return render_template("file.html", filename="hello.txt", filecontent="hello world!")
+@server.route('/', defaults={'path': ''})
+@server.route('/<path:path>')
+def view_file(path):
+    if not pathlib.Path(path).exists():
+        return Response("404 Not Found", status=404, mimetype="text/plain")
+    
+    try:
+        with open(path, "r") as file:
+            content = "\n".join(file.readlines())
 
-server.run(debug=True)
+            try:
+                raw = request.args.get("raw", False, bool)
+                download = request.args.get("download", False, bool)
+
+                if download:
+                    return send_file(path, as_attachment=True)
+                
+                if raw:
+                    return Response(content, status=200, mimetype="text/plain")
+            except ValueError:
+                print("Bad value for raw/download param, ignoring..")
+            
+            return render_template("file.html", filename=path, filecontent=content)
+    except UnicodeDecodeError:
+        try:
+            raw = request.args.get("raw", False, bool)
+            download = request.args.get("download", False, bool)
+        
+            if download or raw:
+                return send_file(path, as_attachment=True)
+        except ValueError:
+            print("Bad value for raw/download param, ignoring..")
+
+        return render_template("file.html", filename=path, filecontent="The file you're requesting isn't a text file, so we can't preview it here.")
+
+server.run()
